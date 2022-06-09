98c1fd625609a51dbdf2d7f9e43b67c4f8160c8c
Brodie Gress <brodielgress@gmail.com>
2022-06-09 16:48:43 -0400

    Added more styles to font, added a light JS function, need to restyle for mobile first

--- JS/coderBrodie.js
+++ JS/coderBrodie.js
@@ -1,3 +1,11 @@
 function game() {
     
 }
+
+let all_hrefs = document.querySelectorAll("a");
+
+all_hrefs.forEach(function(single_href) {
+    single_href.addEventListener('click', function(){
+        this.classList.add('visited');
+    })
+});
